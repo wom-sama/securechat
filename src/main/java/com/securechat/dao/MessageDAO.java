f85b6cdd6972d4fc5d5d4d3d4d62fc37de86f890
ADMIN <ADMIN@LAPTOP-65D3JG04>
2025-12-28 02:33:16 +0700

    add index for message

--- src/main/java/com/securechat/dao/MessageDAO.java
+++ src/main/java/com/securechat/dao/MessageDAO.java
@@ -9,21 +9,46 @@ import com.securechat.config.MongoProvider;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Indexes; 
+import com.mongodb.client.model.IndexOptions; 
 import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MessageDAO {
+    
+     public MessageDAO() {
+        createIndexes();
+    }
+    
     private MongoCollection<Document> col() {
         return MongoProvider.db().getCollection(AppConfig.COL_MESSAGES);
     }
+    //tao index
+    private void createIndexes() {
+        try {
+            MongoCollection<Document> c = col();
+            c.createIndex(Indexes.compoundIndex(
+                Indexes.ascending("to"), 
+                Indexes.descending("ts")
+            ), new IndexOptions().name("idx_inbox"));
+            c.createIndex(Indexes.compoundIndex(
+                Indexes.ascending("from"), 
+                Indexes.descending("ts")
+            ), new IndexOptions().name("idx_sent"));
+            
+        } catch (Exception e) {
+            System.out.println("Index creation warning: " + e.getMessage());
+        }
+    }
+   
 
     public void insertMessage(Document msgDoc) {
         col().insertOne(msgDoc);
     }
 
-    public List<Document> findInbox(String username, int limit) {
+   public List<Document> findInbox(String username, int limit) {
         List<Document> out = new ArrayList<>();
         col().find(Filters.eq("to", username))
                 .sort(Sorts.descending("ts"))
